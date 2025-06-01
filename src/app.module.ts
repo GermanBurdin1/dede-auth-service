0e68531aea0ae7515a1be0366a478c49e7097a3f
German <125369843+GermanBurdin1@users.noreply.github.com>
2025-06-01 17:42:58 +0200

    передаем подробную информацию

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,35 +7,41 @@ import { User } from './users/user.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth.module';
 import { TeacherProfileModule } from './users/teacher/teacher-profile.module';
+import { TeacherProfile } from './users/teacher/teacher_profiles.entity';
+import { TeacherCertificate } from './users/teacher/teacher_certificates.entity';
+import { TeacherSpecialization } from './users/teacher/teacher_specializations.entity';
 
 
 @Module({
-  imports: [
+	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
 
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: +config.get<number>('DB_PORT')!,
-        username: config.get<string>('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
-        entities: [User],
-        migrations: ['dist/migrations/*.js'],
-        synchronize: false,
-      }),
-    }),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (config: ConfigService) => ({
+				type: 'postgres',
+				host: config.get<string>('DB_HOST'),
+				port: +config.get<number>('DB_PORT')!,
+				username: config.get<string>('DB_USERNAME'),
+				password: config.get<string>('DB_PASSWORD'),
+				database: config.get<string>('DB_NAME'),
+				entities: [User,
+					TeacherProfile,
+					TeacherCertificate,
+					TeacherSpecialization],
+				migrations: ['dist/migrations/*.js'],
+				synchronize: false,
+			}),
+		}),
 
-    // MongoDB (Mongoose)
-    MongooseModule.forRoot(process.env.MONGO_URI),
+		// MongoDB (Mongoose)
+		MongooseModule.forRoot(process.env.MONGO_URI),
 
-    UsersModule,
-    ProfilesModule,
+		UsersModule,
+		ProfilesModule,
 		AuthModule,
 		TeacherProfileModule
-  ],
+	],
 })
-export class AppModule {}
+export class AppModule { }
